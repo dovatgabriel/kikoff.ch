99073d6d3a92b9cd5ce4e33a682889e660dec13a
Gabriel <gabriel@optifit.app>
2025-08-28 20:04:31 +0200

    feat: navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { Route, Routes } from 'react-router-dom';
 import { Landing } from './pages/landing';
 import { NotFound } from './pages/not-found';
 import { Footer } from './components/ui/footer';
+import { Navbar } from './components/ui/navbar';
 
 type AppProps = object;
 
 export const App: FC<AppProps> = () => (
   <>
+    <Navbar />
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="*" element={<NotFound />} />
