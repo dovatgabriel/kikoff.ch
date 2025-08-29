3087e9004a110a4f92c6ea4ca8e05d8213d8f6e9
Gabriel <gabriel@optifit.app>
2025-08-29 10:55:12 +0200

    feat: loading state

--- src/components/landing/trending.tsx
+++ src/components/landing/trending.tsx
@@ -1,6 +1,7 @@
 import { useShop } from '@/providers/shop-provider';
 import type { FC } from 'react';
 import { Item } from '../common/item';
+import { Skeleton } from '../ui/skeleton';
 
 type TrendingProps = object;
 
@@ -11,9 +12,19 @@ export const Trending: FC<TrendingProps> = () => {
     <div className="max-w-screen py-20 px-50">
       <h1 className="text-5xl font-bold">Articles du moment</h1>
       <div className="flex items-stretch gap-3 flex-wrap mt-10">
-        {trending.map((item, key) => (
-          <Item item={item} key={key} />
-        ))}
+        {trending.length > 0 ? (
+          <>
+            {trending.map((item, key) => (
+              <Item item={item} key={key} />
+            ))}
+          </>
+        ) : (
+          <>
+            {Array.from({ length: 4 }).map((_, key) => (
+              <Skeleton className="w-[376px] h-[556px] rounded-lg" key={key} />
+            ))}
+          </>
+        )}
       </div>
     </div>
   );
