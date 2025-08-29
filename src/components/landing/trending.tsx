aed985b5230b59963d4725fc46cd0d1b135f7ddf
Gabriel <gabriel@optifit.app>
2025-08-29 10:51:00 +0200

    chore: avoid marquee for trending items

--- src/components/landing/trending.tsx
+++ src/components/landing/trending.tsx
@@ -1,6 +1,5 @@
 import { useShop } from '@/providers/shop-provider';
 import type { FC } from 'react';
-import { Marquee } from '../ui/marquee';
 import { Item } from '../common/item';
 
 type TrendingProps = object;
@@ -9,13 +8,13 @@ export const Trending: FC<TrendingProps> = () => {
   const { trending } = useShop();
 
   return (
-    <div className="max-w-screen py-20">
-      <h1 className="text-5xl font-bold px-50">Articles du moment</h1>
-      <Marquee className="my-10" duration={200}>
+    <div className="max-w-screen py-20 px-50">
+      <h1 className="text-5xl font-bold">Articles du moment</h1>
+      <div className="flex items-stretch gap-3 flex-wrap mt-10">
         {trending.map((item, key) => (
           <Item item={item} key={key} />
         ))}
-      </Marquee>
+      </div>
     </div>
   );
 };
