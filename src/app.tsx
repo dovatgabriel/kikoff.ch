51fd297ba1176a9a2a10fd79737fbffb85a42fed
dmytrobez <dmitrobeznoskovic@gmail.com>
2025-11-27 19:40:01 +0100

    feat: checkout page

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Home } from '@/pages/home';
 import { ProductDetail } from '@/pages/product-detail';
 import { Products } from '@/pages/products';
+import { Checkout } from '@/pages/checkout';
 
 export const App = () => {
   return (
@@ -9,6 +10,7 @@ export const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/products" element={<Products />} />
       <Route path="/product/:id" element={<ProductDetail />} />
+      <Route path="/checkout" element={<Checkout />} />
     </Routes>
   );
 };
