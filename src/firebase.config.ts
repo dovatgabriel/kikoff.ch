56bdf3561e8516a0e142fd7e6b1c7682c01251fc
Gabriel <gabriel@optifit.app>
2025-12-04 07:37:09 +0100

    fix: functions region

--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -13,7 +13,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-export const functions = getFunctions(app);
+export const functions = getFunctions(app, 'us-central1');
 export const auth = getAuth(app);
 
 if (window.location.host === 'localhost:5173') {
